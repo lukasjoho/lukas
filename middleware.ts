1ee895f2a458cdb97b3cd617c3514c3fe2551c7f
lukasjoho <social@lukashoppe.com>
2023-08-27 19:37:12 +0200

    test: add headers to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,44 @@
-import { Kafka } from '@upstash/kafka';
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest, event: NextFetchEvent) {
-  const kafka = new Kafka({
-    url: process.env.UPSTASH_KAFKA_URL,
-    username: process.env.UPSTASH_KAFKA_USERNAME,
-    password: process.env.UPSTASH_KAFKA_PASSWORD,
-  });
+  console.log('EVENT: ', JSON.stringify(event));
+  console.log('REQ: ', JSON.stringify(req.headers.get('Sec-purpose')));
+  console.log('REQ2: ', JSON.stringify(req.headers.get('sec-purpose')));
+  console.log('DEST: ', JSON.stringify(req.headers.get('sec-fetch-dest')));
+  console.log('Purpose: ', JSON.stringify(req.headers.get('Purpose')));
+  console.log('Purpose1: ', JSON.stringify(req.headers.get('purpose')));
+  // const kafka = new Kafka({
+  //   url: process.env.UPSTASH_KAFKA_URL,
+  //   username: process.env.UPSTASH_KAFKA_USERNAME,
+  //   password: process.env.UPSTASH_KAFKA_PASSWORD,
+  // });
 
-  const message = {
-    referrer: req.referrer,
-    country: req.geo?.country,
-    city: req.geo?.region,
-    url: req.url,
-    path: req.nextUrl.pathname,
-    ip: req.headers.get('x-real-ip'),
-    mobile: req.headers.get('sec-ch-ua-mobile'),
-    prefetch: req.headers.get('next-router-prefetch'),
-    purpose: req.headers.get('purpose'),
-    middlewarePrefetch: req.headers.get('x-middleware-prefetch'),
-    nextData: req.headers.get('x-nextjs-data'),
-    platform: req.headers.get('sec-ch-ua-platform'),
-    useragent: req.headers.get('user-agent'),
-    timestamp: new Date(),
-    cookies: req.cookies,
-  };
+  // const message = {
+  //   referrer: req.referrer,
+  //   country: req.geo?.country,
+  //   city: req.geo?.region,
+  //   url: req.url,
+  //   path: req.nextUrl.pathname,
+  //   ip: req.headers.get('x-real-ip'),
+  //   mobile: req.headers.get('sec-ch-ua-mobile'),
+  //   prefetch: req.headers.get('next-router-prefetch'),
+  //   purpose: req.headers.get('purpose'),
+  //   middlewarePrefetch: req.headers.get('x-middleware-prefetch'),
+  //   nextData: req.headers.get('x-nextjs-data'),
+  //   platform: req.headers.get('sec-ch-ua-platform'),
+  //   useragent: req.headers.get('user-agent'),
+  //   timestamp: new Date(),
+  //   cookies: req.cookies,
+  // };
 
-  if (message.useragent?.includes('Vercelbot')) {
-    return NextResponse.next();
-  }
+  // if (message.useragent?.includes('Vercelbot')) {
+  //   return NextResponse.next();
+  // }
 
-  const p = kafka.producer();
-  const topic = 'analytics';
-  console.log('MESSAGE: ', message);
-  event.waitUntil(p.produce(topic, JSON.stringify(message)));
+  // const p = kafka.producer();
+  // const topic = 'analytics';
+  // console.log('MESSAGE: ', message);
+  // event.waitUntil(p.produce(topic, JSON.stringify(message)));
 
   return NextResponse.next();
 }
