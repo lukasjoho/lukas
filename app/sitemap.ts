bf237babb57ebfd6731ab90b8e9a8f127398a6da
lukasjoho <social@lukashoppe.com>
2023-09-25 10:25:42 +0200

    build: extend sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,46 @@
+import {
+  getBlogposts,
+  getCodeProjects,
+  getPhotoProjects,
+  getVideoProjects,
+} from '@/lib/clients/contentful';
+
 export default async function sitemap() {
-  const routes = ['', '/code', '/photo', '/video', '/blog'].map((route) => ({
+  const base = ['', '/code', '/photo', '/video', '/blog'].map((route) => ({
     url: `https://lukashoppe.com${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
-  return [...routes];
+  const codeProjects = await getCodeProjects();
+  const photoProjects = await getPhotoProjects();
+  const videoProjects = await getVideoProjects();
+  const blogPosts = await getBlogposts();
+
+  codeProjects.map((project) => ({
+    url: `https://lukashoppe.com/code/${project.slug}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
+  codeProjects.map((project) => ({
+    url: `https://lukashoppe.com/code/${project.slug}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+  photoProjects.map((project) => ({
+    url: `https://lukashoppe.com/photo/${project.slug}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+  videoProjects.map((project) => ({
+    url: `https://lukashoppe.com/video/${project.slug}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+  blogPosts.map((project) => ({
+    url: `https://lukashoppe.com/blog/${project.slug}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+  return [
+    ...base,
+    ...codeProjects,
+    ...photoProjects,
+    ...videoProjects,
+    ...blogPosts,
+  ];
 }
